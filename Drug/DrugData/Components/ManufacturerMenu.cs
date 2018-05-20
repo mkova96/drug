079a46f955b8943f56f74c44268b6ee1b7c76976
mkova <mario.kovacevic@fer.hr>
2018-05-20 20:09:52 +0200

    proizvodjaci navbar added
    Signed-off-by: mkova <mario.kovacevic@fer.hr>

---
+++ Drug/DrugData/Components/ManufacturerMenu.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrugData.Components
+{
+   public  class ManufacturerMenu:ViewComponent
+    {
+        public ManufacturerMenu()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            List<String> sortList = new List<String>();
+            sortList.Add("Po broju proizvoda silazno");
+            sortList.Add("Po broju proizvoda uzlazno");
+
+            return View(sortList);
+        }
+    }
+}
---
+++ Drug/DrugData/Models/ViewModels/ManufacturersListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrugData.Models.ViewModels
+{
+    public class ManufacturersListViewModel
+    {
+        public IEnumerable<Manufacturer> Manufacturers { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
