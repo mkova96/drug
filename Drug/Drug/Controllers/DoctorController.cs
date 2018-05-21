a08e1291bd5dbd8aa2dcc93c0d67c2d4a86c7f48
mkova <mario.kovacevic@fer.hr>
2018-05-21 17:30:05 +0200

    user delete added
    Signed-off-by: mkova <mario.kovacevic@fer.hr>

--- Drug/Drug/Controllers/DoctorController.cs
+++ Drug/Drug/Controllers/DoctorController.cs
@@ -262,5 +262,36 @@ namespace Lijek.Controllers
                 ModelState.AddModelError(string.Empty, error.Description);
             }
         }
+
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            var user = _databaseContext.User.Find(id);
+
+            var comments = _databaseContext.Comment.Include(i => i.User).Where(t => t.User.Id == id).ToList();
+            foreach (var t in comments)
+            {
+                _databaseContext.Comment.Remove(t);
+            }
+            _databaseContext.SaveChanges();
+
+            var messages = _databaseContext.Message.Include(i => i.Sender).Include(t => t.Receiver).Where(t => (t.Sender.Id == id || t.Receiver.Id == id)).ToList();
+            foreach (var t in messages)
+            {
+                _databaseContext.Message.Remove(t);
+            }
+            _databaseContext.SaveChanges();
+
+            var orders = _databaseContext.Order.Include(i => i.User).Where(t => t.User.Id == id).ToList();
+            foreach (var t in orders)
+            {
+                _databaseContext.Order.Remove(t);
+            }
+            _databaseContext.SaveChanges();
+
+            _databaseContext.Database.ExecuteSqlCommand("DELETE FROM \"AspNetUsers\" WHERE \"AspNetUsers\".\"Id\" = {0}", id);
+            _databaseContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
